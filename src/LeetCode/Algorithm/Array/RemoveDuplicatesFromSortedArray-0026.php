079b6707dc98d2de435fdcb64f009f90a67845f7
wanghaohua <wanghaohua@lianjia.com>
2019-04-17 21:52:03 +0800

    feature:leetcode-26

---
+++ src/LeetCode/Algorithm/Array/RemoveDuplicatesFromSortedArray-0026.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * User: whh935
+ * Date: 2019/4/17 21:29
+ * Desc: LeetCode第26题-https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+ *      删除排序数组中的重复项，返回数组长度
+ */
+
+/**
+  * 思路：快慢指针
+  */
+function removeDuplicates(&$nums)
+{
+    $length = count($nums);
+    if ($length < 2) {
+        return $length;
+    }
+
+    for ($i = 0, $j = 1; $j < $length; $j++) {
+        if ($nums[$i] == $nums[$j]) {
+            continue;
+        }
+        $i++;
+        $nums[$i] = $nums[$j];
+    }
+
+    return $i + 1;
+}
+
+$nums = [0,0,1,1,1,2,2,3,3,4];
+var_dump(removeDuplicates($nums));exit;
