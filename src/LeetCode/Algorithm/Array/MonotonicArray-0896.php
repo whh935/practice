b56ad48cc650919b823781a3f57c565f7c9d7178
wanghaohua <wanghaohua@lianjia.com>
2019-06-19 18:11:22 +0800

    feature:LeetCode-0896

---
+++ src/LeetCode/Algorithm/Array/MonotonicArray-0896.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * User: whh935
+ * Date: 2019/6/19 17:10
+ * Desc: LeetCode第896题-https://leetcode.com/problems/monotonic-array/
+ *      判断数组是否是单调的
+ */
+
+function isMonotonic($A)
+{
+    $length = count($A);
+
+    $increasing = true;
+    $decreasing = true;
+    for ($i = 0; $i < $length - 1; $i++) {
+        if ($A[$i] > $A[$i + 1]) {
+            $increasing = false;
+        }
+
+        if ($A[$i] < $A[$i + 1]) {
+            $decreasing = false;
+        }
+    }
+
+    return $increasing || $decreasing;
+}
+
+$arr = [11,11,9,4,3,3,3,1,-1,-1,3,3,3,5,5,5];
+$arr = [1,3,2];
+$arr = [1,2,3,4,5,5,4,3,2,1];
+
+$arr = [1,2,2,3];
+$arr = [1,1,1];
+var_dump(isMonotonic($arr));exit;
--- src/LeetCode/Algorithm/Array/TwoSumInputArrayIsSorted-0167.php
+++ src/LeetCode/Algorithm/Array/TwoSumInputArrayIsSorted-0167.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * User: whh935
- * Date: 2018/5/23 23:13
+ * Date: 2019/6/18 21:13
  * Desc: LeetCode第167题-https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
  *      给定一个整数且已升序数组，从中找出两个数的下标，使得它们的和等于一个特定的数字。
  *      可以假设题目有唯一解。
