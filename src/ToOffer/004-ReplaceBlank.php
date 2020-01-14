9b3d749490c60832ec9e0798469088959293443b
wanghaohua <wanghaohua@lianjia.com>
2020-01-14 20:34:58 +0800

    feature:剑指offer-替换空格

--- src/ToOffer/004-ReplaceBlank.php
+++ src/ToOffer/004-ReplaceBlank.php
@@ -6,9 +6,36 @@
  *      替换字符串中的空格为%20
  */
 
+/**
+ * @param $str
+ * @return string
+ */
 function replaceBlank($str)
 {
-    return str_replace(' ', '%20', $str);
+    $original_length = strlen($str);
+    if ($original_length <= 0) {
+        return '';
+    }
+
+    $blanks = 0;
+    for ($i = 0; $i < $original_length; $i++) {
+        if ($str[$i] == ' ') {
+            $blanks++;
+        }
+    }
+
+    $new_length = ($original_length + $blanks * 2) - 1;
+    for ($i = $original_length - 1; $i >= 0; $i--) {
+        if ($str[$i] == ' ') {
+            $str[$new_length--] = '0';
+            $str[$new_length--] = '2';
+            $str[$new_length--] = '%';
+        } else {
+            $str[$new_length--] = $str[$i];
+        }
+    }
+
+    return $str;
 }
 
 $str = 'whh is cool!';
