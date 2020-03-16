30a0c4a6e782d9bf696b07ff532466fc4d09f0bb
wanghaohua <wanghaohua@lianjia.com>
2020-03-16 10:34:33 +0800

    feature:leetcode-0002+0033

--- src/LeetCode/Algorithm/Array/0033-SearchInRotatedSortedArray.php
+++ src/LeetCode/Algorithm/Array/0033-SearchInRotatedSortedArray.php
@@ -7,8 +7,15 @@
  */
 
 /**
-  * 思路:借助二分查找的思路，比较$nums[0]和$target的值来判断不同的case
-  */
+ * 题目要求 O(logN)的时间复杂度，基本可以断定本题是需要使用二分查找，怎么分是关键。
+ * 由于题目说数字了无重复，举个例子：
+ * 1 2 3 4 5 6 7 可以大致分为两类，
+ * 第一类 2 3 4 5 6 7 1 这种，也就是 nums[start] <= nums[mid]。此例子中就是 2 <= 5。
+ * 这种情况下，前半部分有序。因此如果 nums[start] <=target<nums[mid]，则在前半部分找，否则去后半部分找。
+ * 第二类 6 7 1 2 3 4 5 这种，也就是 nums[start] > nums[mid]。此例子中就是 6 > 2。
+ * 这种情况下，后半部分有序。因此如果 nums[mid] <target<=nums[end]，则在后半部分找，否则去前半部分找。
+ */
+
 /**
  * @param $nums
  * @param $target
@@ -21,23 +28,25 @@ function search($nums, $target)
         return -1;
     }
 
-    $left  = 0;
-    $right = $length - 1;
-    while ($left <= $right) {
-        $mid = intval($left + ($right - $left) / 2);
+    $start  = 0;
+    $end = $length - 1;
+    while ($start <= $end) {
+        $mid = intval($start + ($end - $start) / 2);
         if ($nums[$mid] == $target) {
             return $mid;
         }
 
-        if ($target >= $nums[0] && $nums[0] > $nums[$mid]) {
-            $right = $mid - 1;
-        } elseif ($target < $nums[0] && $nums[0] <= $nums[$mid]) {
-            $left = $mid + 1;
-        } else {
-            if ($nums[$mid] > $target) {
-                $right = $mid - 1;
+        if ($nums[$start] <= $nums[$mid]) {//前半段都是有序的
+            if ($nums[$start] <= $target && $target < $nums[$mid]) {
+                $end = $mid - 1;
             } else {
-                $left = $mid + 1;
+                $start = $mid + 1;
+            }
+        } else {
+            if ($nums[$mid] < $target && $target <= $nums[$end]) {
+                $start = $mid + 1;
+            } else {
+                $end = $mid - 1;
             }
         }
     }
@@ -45,11 +54,7 @@ function search($nums, $target)
     return -1;
 }
 
-
 $nums = [4,5,6,7,0,1,2];
 $target = 0;
 
-//$nums = [1,3];
-//$target = 1;
 var_dump(search($nums, $target));
-exit;
