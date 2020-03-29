1f5603ae6495eb6a72142d32b4a1ea9e88e03c26
wanghaohua <wanghaohua@lianjia.com>
2020-03-29 19:42:35 +0800

    feature:LeetCode-0094+0144+0145

---
+++ src/LeetCode/Algorithm/Tree/0094-InorderTraversal.php
@@ -0,0 +1,89 @@
+<?php
+/**
+ * User: whh935
+ * Date: 2020/3/29 15:04
+ * Desc: LeetCode第94题-https://leetcode-cn.com/problems/binary-tree-inorder-traversal/
+ *      给定一个二叉树，返回它的 中序 遍历。
+ *      输入: [1,null,2,3]
+ *      1
+ *       \
+ *        2
+ *       /
+ *      3
+ *      输出: [1,3,2]
+ */
+
+class BinaryTreeNode
+{
+    public $left = null;
+    public $right = null;
+    public $val = '';
+
+    /**
+     * BinaryTreeNode constructor.
+     * @param $value
+     */
+    public function __construct($value)
+    {
+        $this->val = $value;
+        return $this;
+    }
+
+    /**
+     * 构建二叉树
+     * @param BinaryTreeNode|null $left_child
+     * @param BinaryTreeNode|null $right_child
+     */
+    public function buildTree(BinaryTreeNode $left_child = null, BinaryTreeNode $right_child = null)
+    {
+        if (!is_null($left_child)) {
+            $this->left = $left_child;
+        }
+        if (!is_null($right_child)) {
+            $this->right = $right_child;
+        }
+    }
+}
+
+/**
+ * 利用栈中序遍历二叉树，与前序遍历不同的是，出栈时才将结果写入列表
+ * @param $root
+ * @return array
+ */
+function inorderTraversal($root)
+{
+    if ($root == null) {
+        return [];
+    }
+
+    $result = [];
+    $stack = [];
+    $curr = $root;
+    while (!empty($stack) || $curr != null) {
+        while ($curr != null) {
+            array_push($stack, $curr);
+            $curr = $curr->left;
+        }
+
+        $node = array_pop($stack);//此时左子树遍历完成
+        $result[] = $node->val;//将根节点加入列表
+
+        $curr = $node->right;//遍历右子树
+    }
+
+    return $result;
+}
+
+$a = new BinaryTreeNode(1);
+$b = new BinaryTreeNode(2);
+$c = new BinaryTreeNode(3);
+$d = new BinaryTreeNode(4);
+$e = new BinaryTreeNode(5);
+$f = new BinaryTreeNode(6);
+
+$a->buildTree($b, $c);
+$b->buildTree($d, $e);
+$c->buildTree($f, null);
+
+$result = inorderTraversal($a);
+var_dump($result);
---
+++ src/LeetCode/Algorithm/Tree/0145-PostorderTraversal.php
@@ -0,0 +1,90 @@
+<?php
+/**
+ * User: whh935
+ * Date: 2020/3/29 17:48
+ * Desc: LeetCode第145题-https://leetcode-cn.com/problems/binary-tree-postorder-traversal/
+ *      给定一个二叉树，返回它的 后序 遍历。
+ *      输入: [1,null,2,3]
+ *      1
+ *       \
+ *        2
+ *       /
+ *      3
+ *      输出: [3,2,1]
+ */
+
+class BinaryTreeNode
+{
+    public $left = null;
+    public $right = null;
+    public $val = '';
+
+    /**
+     * BinaryTreeNode constructor.
+     * @param $value
+     */
+    public function __construct($value)
+    {
+        $this->val = $value;
+        return $this;
+    }
+
+    /**
+     * 构建二叉树
+     * @param BinaryTreeNode|null $left_child
+     * @param BinaryTreeNode|null $right_child
+     */
+    public function buildTree(BinaryTreeNode $left_child = null, BinaryTreeNode $right_child = null)
+    {
+        if (!is_null($left_child)) {
+            $this->left = $left_child;
+        }
+        if (!is_null($right_child)) {
+            $this->right = $right_child;
+        }
+    }
+}
+
+/**
+ * 借助先序遍历的思路，得到根>右>左，再翻转一次
+ * 利用栈后序遍历二叉树
+ * @param $root
+ * @return array
+ */
+function postorderTraversal($root)
+{
+    if ($root == null) {
+        return [];
+    }
+
+    $result = [];
+    $stack = [];//辅助栈
+    $curr = $root;//当前节点
+    while (!empty($stack) || $curr != null) {
+        while ($curr != null) {//遍历到最后一层
+            $result[] = $curr->val;
+            array_push($stack, $curr);
+            $curr = $curr->right;
+        }
+
+        $node = array_pop($stack);//此时该节点的右子树已经全部遍历完
+        $curr = $node->left;//对左子树遍历
+    }
+
+    $result = array_reverse($result);
+    return $result;
+}
+
+$a = new BinaryTreeNode(1);
+$b = new BinaryTreeNode(2);
+$c = new BinaryTreeNode(3);
+$d = new BinaryTreeNode(4);
+$e = new BinaryTreeNode(5);
+$f = new BinaryTreeNode(6);
+
+$a->buildTree($b, $c);
+$b->buildTree($d, $e);
+$c->buildTree($f, null);
+
+$result = postorderTraversal($a);
+var_dump($result);
